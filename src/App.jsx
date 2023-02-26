5d50451a5b97c92e28bd1db75b030c3a0a74106f
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 17:13:07 +0100

    basic dependenciess added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
---
+++ src/components/pages/LoggedInHome.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const LoggedInHome = () => {
+  return <div>logged in home</div>;
+};
