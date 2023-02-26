b77459bea85173ca6ba1d30ca79e8802984c2251
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 18:46:55 +0100

    login page layout built

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="w-1/4 m-auto text-center">
+      <h1 className="text-3x1 my-3 font-bold">Login</h1>
+      <form action="">
+        <div className="mb-3">
+          <input
+            type="email"
+            placeholder="insert valid email..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            type="password"
+            placeholder="insert password..."
+            className="focus:outline-none border-none p-2 rounded w-full rounded"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="bg-black text-white w-full py-2">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
