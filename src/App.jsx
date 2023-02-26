466e63967e6396ea3ae1ed7c6961a60c448c3e83
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 17:59:42 +0100

    basic folder and files structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Navbar } from './components/Navbar';
+// import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar';
+import { Home } from './components/pages/Home';
+import { Register } from './components/pages/Register';
+import { Profile } from './components/pages/Profile';
+import { Login } from './components/pages/Login';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/register" element={<Register />} />
+        <Route path="/user/login" element={<Login />} />
+        <Route path="/user/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-black py-3 text-white flex items-center justify-between  ">
+      <h1 className="ml-1 font-bold text-2xl">
+        Things I want to do before I die
+      </h1>
+      <ul className="flex">
+        <li className="mr-3">
+          <Link className="hover:text-pink-500" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="mr-3">
+          <Link className="hover:text-pink-500" to="/todo/create">
+            Add task
+          </Link>
+        </li>
+        <li className="mr-3">
+          <Link className="hover:text-pink-500" to="user/register">
+            Register
+          </Link>
+        </li>
+        <li className="mr-3">
+          <Link className="hover:text-pink-500" to="user/login">
+            Login
+          </Link>
+        </li>
+        <li className="mr-3">
+          <Link className="hover:text-pink-500" to="user/profile">
+            Profile
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
