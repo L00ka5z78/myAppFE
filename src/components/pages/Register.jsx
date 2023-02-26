928ea98f20a4e5a557cfdcd6885f1fa64434a5d2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 18:13:05 +0100

    navbar with links created

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,5 +1,47 @@
 import React from 'react';
 
 export const Register = () => {
-  return <div>register</div>;
+  return (
+    <div className="w-1/4 m-auto text-center">
+      <h1 className="text-3x1 my-3 font-bold">Register</h1>
+      <form action="">
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="insert name..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            placeholder="insert valid email..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            type="number"
+            placeholder="insert age..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            placeholder="insert password..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            placeholder="confirm password..."
+            className="focus:outline-none border-none p-2 rounded w-full"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
