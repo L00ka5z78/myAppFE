6f0e1c2bf58b73c7c345c44bd0253f07d177415d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-03-12 08:36:20 +0100

    JS to TS migration types added

---
+++ src/types/todoType.ts
@@ -0,0 +1,26 @@
+export interface ITodo {
+    id: string;
+    _id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+    //   user: {
+    //     type: mongoose.Schema.Types.ObjectId;
+    //     ref: 'User';
+    //     required: true;
+    //   };
+    user: any;
+    createdAt?: string;
+    updatedAt?: string;
+}
+
+export interface TodoProps {
+    todo: ITodo;
+}
+
+export type ApiDataType = {
+    message: string;
+    status: string;
+    todos: ITodo[];
+    todo?: ITodo;
+};
